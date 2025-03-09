f350f3e44c08c8af2a4463c3c0a83937e4a9d87d
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-03-09 21:11:03 +0200

    feat(posts page): fetch posts from API, create grid container for posts and Post component and finish it's style

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,18 @@
+import Post from '@/components/Post'
 import React from 'react'
 
-export default function Posts() {
+export default async function Posts() {
+
+  const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+
   return (
-    <div>Posts</div>
+    <div className="container">
+      <h1 className='text-base py-8 sm:text-4xl'>Posts</h1>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-3 pb-8">
+        {posts.map((post: any) => (
+          <Post key={post.id} post={post}></Post>
+        ))}
+      </div>
+    </div>
   )
 }
