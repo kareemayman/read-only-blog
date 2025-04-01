c9a63643374c5141ef98a880d8c4c27e971b6567
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-04-02 00:16:02 +0200

    feat(user): create specific user page, and fetch user posts

---
+++ src/app/users/[id]/page.tsx
@@ -0,0 +1,45 @@
+import Post from "@/components/Post"
+import { Metadata } from "next"
+import React from "react"
+
+export const metadata: Metadata = {
+  title: "User Page",
+}
+
+export default async function UserPage({ params }: { params: { id: string } }) {
+  const user = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`).then((res) =>
+    res.json()
+  )
+
+  const userPosts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${params.id}`).then((res) => res.json())
+
+  return (
+    <div className="container">
+      <h1 className="text-3xl pt-4 sm:text-4xl sm:pt-8">{user.name}</h1>
+      <p className="py-4 sm:text-xl">{user.email}</p>
+      <div className="info">
+        <p>
+          <strong>Company: </strong>
+          {user.company.name}
+        </p>
+        <p>
+          <strong>Website: </strong>
+          {user.website}
+        </p>
+        <p>
+          <strong>Address: </strong>
+          {user.address.street} {user.address.suite} {user.address.city} {user.address.zipcode}
+        </p>
+      </div>
+
+      <div className="posts my-4 sm:my-8">
+        <p className="font-black text-xl">Posts</p>
+        <div className="posts-wrapper grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-3 py-4">
+            {userPosts.map((post: any) => {
+                return <Post key={post.id} post={post}></Post>
+            })}
+        </div>
+      </div>
+    </div>
+  )
+}
